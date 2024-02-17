854f7de17dcd77f1751e84c63e9e30f62740ac73
mdmishrat13 <mdmishrat13@gmail.com>
2024-02-17 03:22:56 -0800

    'added'

--- lib/prismaDb.ts
+++ lib/prismaDb.ts
@@ -1,13 +1,5 @@
-import {PrismaClient} from '@prisma/client'
-
-declare global {
-    var prisma : PrismaClient | undefined
-}
-
-const prismaDb = globalThis.prisma || new PrismaClient();
-
-if(process.env.NODE_ENV !== 'development') globalThis.prisma = prismaDb
-
+import { PrismaClient } from '@prisma/client'
+const prismaDb = new PrismaClient()
 
 export default prismaDb;
 
